b4c715b9c9fabf8527f6b3d9f108740a21cc9e6f
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:53:00 +0530

    Create Exercise 107 - Data Type Conversions.py

---
+++ Exercise 107 - Data Type Conversions.py
@@ -0,0 +1,7 @@
+#Exercise 107 - Data Type Conversions
+#Given the code below, use the correct function on line 3 in order to convert value to a binary representation.
+value = 10
+ 
+conv = bin(value)
+ 
+print(conv)
