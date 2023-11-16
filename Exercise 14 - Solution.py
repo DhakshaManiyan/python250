5443035eb8e8940c17a3778c4f2a05dd4bd62ed9
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 18:39:43 +0530

    Create Exercise 14 - Solution.py

---
+++ Exercise 14 - Solution.py
@@ -0,0 +1,8 @@
+#Exercise 14 - Strings
+#Given the code below, insert the correct code on line 4 in order to concatenate my_string with the following string:
+#my_other_string = "Poor guy!"
+
+my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
+my_other_string = "Poor guy!"
+ 
+print(my_string + my_other_string)
