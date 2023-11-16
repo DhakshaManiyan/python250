c44070502bb99cc9dca2f445cf4c732892650724
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 18:31:06 +0530

    Update Exercise 1 - Solution.py

--- Exercise 1 - Solution.py
+++ Exercise 1 - Solution.py
@@ -1,5 +1,5 @@
-Exercise 1 - Strings
-Given the code below, insert the correct negative index on line 3 in order to get the last character in the string.
+#Exercise 1 - Strings
+#Given the code below, insert the correct negative index on line 3 in order to get the last character in the string.
 my_string = "In 2010, someone paid 10k Bitcoin for two pizzas."
  
 print(my_string[-1])
