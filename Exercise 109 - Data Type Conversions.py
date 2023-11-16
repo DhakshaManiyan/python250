ac9a990cfd49ab04f42a447ccd7cd331a7965e20
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:54:32 +0530

    Create Exercise 109 - Data Type Conversions.py

---
+++ Exercise 109 - Data Type Conversions.py
@@ -0,0 +1,7 @@
+Exercise 109 - Data Type Conversions
+Given the code below, use the correct function on line 3 in order to convert value from binary to decimal notation.
+value = '0b1010'
+ 
+conv = int(value, 2)
+ 
+print(conv)
