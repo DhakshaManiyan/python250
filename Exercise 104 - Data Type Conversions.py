e05ca09233613e2bd77b78312b9b97bda74fefb3
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:50:14 +0530

    Create Exercise 104 - Data Type Conversions.py

---
+++ Exercise 104 - Data Type Conversions.py
@@ -0,0 +1,7 @@
+#Exercise 104 - Data Type Conversions
+#Given the code below, use the correct function on line 3 in order to convert value to a list.
+value = "Hello!"
+ 
+conv = list(value)
+ 
+print(type(conv))
