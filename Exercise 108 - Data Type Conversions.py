cb55ae17455d7d069b4f065e6c2ac83e89f121e2
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:53:37 +0530

    Create Exercise 108 - Data Type Conversions.py

---
+++ Exercise 108 - Data Type Conversions.py
@@ -0,0 +1,7 @@
+#Exercise 108 - Data Type Conversions
+#Given the code below, use the correct function on line 3 in order to convert value to a hexadecimal representation.
+value = 10
+ 
+conv = hex(10)
+ 
+print(conv)
