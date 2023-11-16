ec2a8841ecbabf2df9fef9af281a6e3df5447034
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:49:36 +0530

    Create Exercise 103 - Data Type Conversions.py

---
+++ Exercise 103 - Data Type Conversions.py
@@ -0,0 +1,7 @@
+#Exercise 103 - Data Type Conversions
+#Given the code below, use the correct function on line 3 in order to convert value to a floating-point number.
+value = 10
+ 
+conv = float(value)
+ 
+print(type(conv))
