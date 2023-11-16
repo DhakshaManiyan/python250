60fce620c98724122c22c7139902df3c4e89726c
DhakshaManiyan <121249364+DhakshaManiyan@users.noreply.github.com>
2023-11-16 20:30:46 +0530

    Create Exercise 81 - Ranges.py

---
+++ Exercise 81 - Ranges.py
@@ -0,0 +1,5 @@
+#Exercise 81 - Ranges
+#Given the code below, use the correct argument(s) for the range() function on line 1 in order to return [10, 13, 16, 19] when converted to a list.
+my_range = range(10, 20, 3)
+ 
+print(list(my_range)) #[10, 13, 16, 19]
